91b6283e1973a35a6b7f1b3a9b84394df0272bbe
Dominique3d <84606101+Dominique3d@users.noreply.github.com>
2024-02-05 23:08:47 -0300

    Update2 api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://proyecto-titulo-back.vercel.app/";
+export const BASE_URL = "https://proyecto-titulo-back.vercel.app/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
